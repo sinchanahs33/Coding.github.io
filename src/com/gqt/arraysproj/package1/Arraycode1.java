0c5e6be51972876f90e0ba80b7f1d4e07baeb54c
Sinchana <Sinchana@LAPTOP-0VL3CLR3>
2023-04-01 17:27:29 +0530

    array1

--- src/com/gqt/arraysproj/package1/Arraycode1.java
+++ src/com/gqt/arraysproj/package1/Arraycode1.java
@@ -14,10 +14,70 @@ public class Arraycode1 {
 		}
 	    for(int i=0; i<=n-1 ; i++){
 			System.out.println(" the marks of student no:" + (i + 1) + " is:" +arry[i]);
-
-
+	    }
+	    }
 	}
 
+	/*System.out.println("Enter the count of classes:");
+int cls = sc.nextInt();
+System.out.println("Enter the count of students:");
+int stu = sc.nextInt();
+int arry [] []= new int [cls] [stu];
+for(int i=0; i<cls ; i++){
+	System.out.println("Inside the class:" + (i + 1));
+	for(int j=0; j<stu ; j++){
+		System.out.println("Enter the marks of the student no:" + (j + 1));
+		arry[i][j]=sc.nextInt();
 	}
 }
+		for(int i=0; i<cls ; i++){
+			System.out.println("Inside the class:" + (i + 1));
+			for(int j=0; j<stu ; j++){
+				System.out.println(" the marks of the student no:" + (j + 1)+"is:" +arry [i][j]);
+		
 
+}
+		}
+}
+}
+*/
+	/*System.out.println("Enter the count of school:");
+	int sch = sc.nextInt();
+	System.out.println("Enter the count of classes:");
+	int cls = sc.nextInt();
+	System.out.println("Enter the count of students:");
+	int stu = sc.nextInt();
+	int arry [] [] [] = new int [sch] [cls] [stu];
+	for(int i=0; i<sch ; i++){
+		System.out.println("Inside the school:" + (i + 1));
+	
+		for(int j=0; j<cls ; j++){
+			System.out.println("Inside the class:" + (j + 1));
+		
+			for(int k=0; k<stu ; k++){
+				System.out.println("Enter the marks of the student:" + (k + 1));
+			arry [i][j][k]= sc.nextInt();
+			}
+		}
+	}
+	
+    for(int i=0; i<sch ; i++){
+				System.out.println("Inside the school:" + (i + 1));
+			
+				for(int j=0; j<cls ; j++){
+					System.out.println("Inside the class:" + (j + 1));
+				
+					for(int k=0; k<stu ; k++){
+						System.out.println(" the marks of the student no:" + (k + 1) + "is:" + arry[i][j][k]);
+					
+			}
+}
+    }
+}
+}
+*/
+
+	
+
+
+	
